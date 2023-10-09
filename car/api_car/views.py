920be8bb5e5892b0bb90d613ab0cad2b12d5a551
Кирилл <ki9bulygin@icloud.com>
2023-10-09 19:34:49 +0300

    apiV2.1

--- car/api_car/views.py
+++ car/api_car/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 
 from .serializers import (CountrySerializers, CarSerializers,
                           ManufacturerSerializers, CommentSerializers)
@@ -8,18 +9,22 @@ from review.models import Country, Manufacturer, Car, Comment
 class CountryViewSet(viewsets.ModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializers
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class ManufacturerViewSet(viewsets.ModelViewSet):
     queryset = Manufacturer.objects.all()
     serializer_class = ManufacturerSerializers
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializers
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializers
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
