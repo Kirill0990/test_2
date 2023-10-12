e76b168077428b9d0ff3774825a4586254ed5b18
Кирилл <ki9bulygin@icloud.com>
2023-10-12 14:44:41 +0300

    serializers

--- car/api_car/serializers.py
+++ car/api_car/serializers.py
@@ -4,29 +4,29 @@ from review.models import Country, Manufacturer, Car, Comment
 
 
 class CountrySerializers(serializers.ModelSerializer):
-
     class Meta():
         model = Country
-        fields = ('id', 'name', 'manufacturers')
-
-
-class ManufacturerSerializers(serializers.ModelSerializer):
-
-    class Meta():
-        model = Manufacturer
-        fields = ('id', 'name', 'country', 'cars', 'comments')
-
+        fields = ('id', 'name', 'manufacturer')
 
 class CarSerializers(serializers.ModelSerializer):
-
+    count_comments = serializers.SerializerMethodField()
     class Meta():
         model = Car
         fields = ('id', 'name', 'manufacturer',
-                  'start_year', 'end_year', )
+                  'start_year', 'end_year','comments', 'count_comments')
+    def get_count_comments(self, obj):
+        return obj.comments.count()
+
+class ManufacturerSerializers(serializers.ModelSerializer):
+    cars = CarSerializers(many=True)
+    class Meta():
+        model = Manufacturer
+        fields = ('id', 'name', 'country', 'cars',)
+
+
 
 
 class CommentSerializers(serializers.ModelSerializer):
-
     class Meta():
         model = Comment
         fields = ('id', 'email', 'car', 'creation_date', 'comment',)
